dc8e0d3c9828a75e55b2e70515237d3d6d0f19a9
sagnik mondal <sagnikmondal011@gmail.com>
2025-07-22 23:54:21 +0530

    modify

--- javaScript/DOM_2/app.js
+++ javaScript/DOM_2/app.js
@@ -1,15 +1,35 @@
-let btn=document.querySelector("button");
-console.dir(btn);
+// onclick
 
+// select single button
+// let btn=document.querySelector("button");
+// console.dir(btn);
 
 // btn.onclick=function(){
 //     alert("button was clicked");
 // };
 
-
 // another process
 
-function sayhellow(){
-    alert("hellow !");
+// function sayhellow(){
+//     alert("hellow !");
+// };
+// btn.onclick=sayhellow;
+
+
+// multiple button select
+// let btn=document.querySelectorAll("button");
+// for(btns of btn){
+//     btns.onclick=sayhellow;
+// };
+
+
+// onmouse enter
+
+let butt=document.querySelectorAll("button");
+
+function hellow(){
+    alert("hiii");
 };
-btn.onclick=sayhellow;
+for(btns of butt){
+    btns.onmouseenter=hellow;
+};
