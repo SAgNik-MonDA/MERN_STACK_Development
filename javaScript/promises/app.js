c15d21d344d9b68d84881898b8395aceb9515989
sagnik mondal <sagnikmondal011@gmail.com>
2025-07-28 23:44:24 +0530

    file add

---
+++ javaScript/promises/app.js
@@ -0,0 +1,29 @@
+// resolve & rejecct
+
+function savetodb(data){
+return new Promise((resolve,reject)=>{
+let internetspeed=Math.floor(Math.random()*10)+1;
+if(internetspeed > 4){
+    resolve("sucess : data was saved");
+}
+else{
+    reject("failure : weak connection");
+}
+});
+}
+
+
+
+// then & catch
+
+let request=savetodb("sagnik"); //request=promise object
+request
+    .then(()=>{
+        console.log("promise was resolved");
+        console.log(request);
+    })
+    .catch(()=>{
+    console.log("promise was rejected");
+     console.log(request);
+});
+
