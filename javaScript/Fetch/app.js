dc837d595cb20d14aaa25f90067386ef7d2a1907
sagnik mondal <sagnikmondal011@gmail.com>
2025-07-31 00:57:21 +0530

    modify

--- javaScript/Fetch/app.js
+++ javaScript/Fetch/app.js
@@ -1,4 +1,16 @@
+let btn=document.querySelector("button");
+btn.addEventListener("click",async ()=>{
+    let fact=await getFacts();
+    // console.log(fact);
+    let p=document.querySelector("#result");
+    p.innerText =fact;
+    
+})
+
+
+
 let url="https://catfact.ninja/fact";
+
 // fetch(url)
 // .then((res)=>{
 //     console.log(res);
@@ -14,9 +26,25 @@ let url="https://catfact.ninja/fact";
 
 // using async & await
 
-async function getFacts(){
-    let res=await fetch (url);
-    let data =await res.json();
-    console.log(data.fact);
-};
-getFacts();
+// async function getFacts(){
+//     let res=await fetch (url);
+//     let data =await res.json();
+//     console.log(data.fact);
+// };
+// getFacts();
+
+
+
+
+// Axios
+
+// async function getFacts(){
+//    try{
+//     let res=await axios.get(url);
+//     return res.data.fact;
+//    }
+//    catch(e){
+//     console.log("error",e);
+//     return "NO FACT FOUND"
+//    }
+// };
