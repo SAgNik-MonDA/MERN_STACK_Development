35fbee3aa3dafe04b33d3b19f979aa029cc761fb
sagnik mondal <sagnikmondal011@gmail.com>
2025-08-05 23:59:07 +0530

    modify

--- BACKEND/script.js
+++ BACKEND/script.js
@@ -1,4 +1,10 @@
-let n=5;
-for(let i=0;i<n;i++){
-    console.log("hellow",i);
+// let n=5;
+// for(let i=0;i<n;i++){
+//     console.log("hellow",i);
+// };
+
+
+let args=process.argv;
+for(let i=2;i<args.length;i++){
+    console.log("hellow & welcome to",args[i]);
 };
