233b09812a3f34149d3dab8eaf497c8a3796e276
sagnik mondal <sagnikmondal011@gmail.com>
2025-08-06 21:01:22 +0530

    modify

--- BACKEND/ExpressDir/index.js
+++ BACKEND/ExpressDir/index.js
@@ -1,8 +1,41 @@
+
 const express=require("express");
 const app=express();
-console.dir(app);
+
+// console.dir(app);
 
 let port=3000;
+
+// listen
 app.listen(port, ()=>{
     console.log(`app is listening on port ${port}`);
+});
+
+// request & response
+
+// app.use((req,res)=>{
+//     // console.log(req);
+//     console.log("request recived");
+
+
+//     // send request
+
+//     res.send("this is a basic response");
+// });
+
+
+app.get("/", (req,res)=>{
+    res.send("you contacted root path");
+});
+
+app.get("/apple", (req,res)=>{
+    res.send("you contacted apple path");
+});
+
+app.get("/orange", (req,res)=>{
+    res.send("you contacted orange path");
+});
+
+app.get("*", (req,res)=>{
+    res.send("this path does not exist");
 });
