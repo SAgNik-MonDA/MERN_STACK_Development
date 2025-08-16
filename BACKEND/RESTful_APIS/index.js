227054772810abdd2b29516e522c8ae973387143
sagnik mondal <sagnikmondal011@gmail.com>
2025-08-17 04:02:58 +0530

    add file

--- BACKEND/RESTful_APIS/index.js
+++ BACKEND/RESTful_APIS/index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
+
+app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.set("view engine","ejs");
@@ -12,15 +15,18 @@ app.use(express.static(path.join(__dirname,"public")));
 
 let posts=[
     {
+    id : uuidv4(),
     username : "HIT",
     content : "I love coding"
 },
 {
+    id : uuidv4(),
     username : "sagnik",
     content : "hard wokr is important to achive sucess"
 },
 {
-     username : "deba",
+    id : uuidv4(),
+    username : "deba",
     content : "I got selected for my first internship.....!!!!"
 },
 ];
@@ -30,8 +36,43 @@ app.get("/posts",(req , res) => {
 });
 
 
+app.get("/posts/new",(req , res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts",(req,res)=>{
+    let {username,content} = req.body;
+    let id = uuidv4();
+    posts.push({id,username,content});
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id",(req,res)=>{
+    let {id} =req.params;
+    console.log(id);
+    let post=posts.find((p)=>
+        id === p.id
+    );
+    console.log(post);
+    res.render("show.ejs",{post});
+});
+
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+
+    // let newContent = req.body.content; 
+    // console.log(newContent);
+    console.log(id);
+    res.send("patch request working");
+});
+
+
+
+
 
 
 app.listen(port,()=>{
     console.log("listening to port : 8080");
 });
+
