193f82a2ac84a2ca820b501c423e4d227eae929a
sagnik mondal <sagnikmondal011@gmail.com>
2025-08-17 23:56:52 +0530

    add file

--- BACKEND/RESTful_APIS/index.js
+++ BACKEND/RESTful_APIS/index.js
@@ -92,6 +92,16 @@ app.get("/posts/:id/edit",(req,res)=>{
 });
 
 
+app.delete("/posts/:id",(req,res)=>{
+     let { id } = req.params;
+      posts=posts.filter((p)=>
+        id !== p.id
+    );
+    // res.send("delete  sucess");
+     res.redirect("/posts");
+});
+
+
 
 app.listen(port,()=>{
     console.log("listening to port : 8080");
