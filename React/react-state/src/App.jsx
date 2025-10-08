1b7e85973802af4cc1ef0ef3a1d1829714e2b59f
sagnik mondal <sagnikmondal011@gmail.com>
2025-10-08 22:21:00 +0530

    modify 25

--- React/react-state/src/App.jsx
+++ React/react-state/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import Counter from './Counter.jsx';
 import LikeButton from './LikeButton.jsx';
+import LudoBoard from './LudoBoard.jsx';
 
 function App() {
   return (
     <>
-    <h1>States in react</h1>
-    <Counter />
+    {/* <h1>States in react</h1> */}
+    {/* <Counter /> */}
 
     {/* <LikeButton /> */}
+
+    <LudoBoard/>
     </>
   );
 }
