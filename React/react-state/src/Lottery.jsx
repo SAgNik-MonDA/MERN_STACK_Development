a8a37bb1ba2c5f8378aa8275b1575aac9a5a6702
sagnik mondal <sagnikmondal011@gmail.com>
2025-10-10 23:27:28 +0530

    modify 33 & add file

--- React/react-state/src/Lottery.jsx
+++ React/react-state/src/Lottery.jsx
@@ -1,8 +1,18 @@
+import {useState} from "react";
+import "./Lottery.css";
+import {genTicket} from "./helper(Lottery)";
+
+
 export default function Lattery(){
+    let [ticket,setTicket] = useState(genTicket(3));
     return (
         <div>
             <h1>Lottery Game !</h1>
-            
+            <div className="ticket">
+                <span>{ticket[0]}</span>
+                <span>{ticket[1]}</span>
+                <span>{ticket[2]}</span>
+            </div>
         </div>
-    )
+    );
 }
