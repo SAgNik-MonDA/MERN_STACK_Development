37228924834a578e3c9f6df3d05adc0613503b57
sagnik mondal <sagnikmondal011@gmail.com>
2025-10-15 02:23:20 +0530

    modify 45 & add file

---
+++ React/Use-Effect/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState  } from 'react'
+import './App.css'
+import Counter from './Counter'
+import Joker from './Joker'
+
+function App() {
+
+
+  return (
+    <>
+      {/* <Counter /> */}
+
+      <Joker />
+    </>
+  )
+};
+
+export default App
---
+++ React/Use-Effect/src/Counter.jsx
@@ -0,0 +1,30 @@
+import { useState , useEffect } from "react";
+export default function Counter(){
+    let [countx,setCountx] = useState(0);
+     let [county,setCounty] = useState(0);
+
+let inCountx = () =>{
+    setCountx(currCount => currCount + 1);
+};
+
+let inCounty = () =>{
+    setCounty(currCount => currCount + 1);
+};
+
+useEffect(function printSomething(){
+    console.log("this is a side-effect");
+},[countx]); 
+
+// (when we pass [] empty array then side effect arise only render time not re-render time)
+
+
+    return (
+        <div>
+            <h3>countx = {countx}</h3>
+            <button onClick={inCountx}>+1</button>
+
+             <h3>county = {county}</h3>
+            <button onClick={inCounty}>+1</button>
+        </div>
+    )
+}
