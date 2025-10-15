e980c6c7ade0a639f459efd68dc25f39764ae1c1
sagnik mondal <sagnikmondal011@gmail.com>
2025-10-15 20:50:32 +0530

    modify 47 & add file

--- React/React-Mini-Project/Material UI/src/App.jsx
+++ React/React-Mini-Project/Material UI/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
+import Alert from '@mui/material/Alert';
 
 
 function App() {
@@ -16,6 +17,8 @@ let handleClick = ()=>{
       <Button variant="contained" onClick={handleClick} startIcon={<DeleteIcon />}> Delete </Button>
     
           {/* <Button variant="contained"   onClick={handleClick} color="error" size="small"> click me2 </Button> */}
+
+          <Alert severity="error">Delete option is given</Alert>
     </>
   );
 }
