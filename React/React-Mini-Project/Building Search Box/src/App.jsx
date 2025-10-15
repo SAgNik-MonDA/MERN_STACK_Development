ee448a6974981e62881bcd2c8d7f7db6e9e0bdfc
sagnik mondal <sagnikmondal011@gmail.com>
2025-10-16 02:31:21 +0530

    modify 51 & add file

--- React/React-Mini-Project/Building Search Box/src/App.jsx
+++ React/React-Mini-Project/Building Search Box/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 // import './App.css'
-import SearchBox from './SearchBox'
+import WeatherApp from './WeatherApp'
 
 function App() {
   
   return (
     <>
-    <SearchBox />
+    <WeatherApp />
+    
     </>
   )
 }
--- React/React-Mini-Project/Building Search Box/src/SearchBox.jsx
+++ React/React-Mini-Project/Building Search Box/src/SearchBox.jsx
@@ -2,23 +2,59 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import "./SearchBox.css";
 import { useState } from 'react';
-export default function SearchBox(){
+export default function SearchBox({updateInfo}){
     let[city,setCity]=useState("");
+    let[error,setError]=useState(false);
+
+const API_URL="https://api.openweathermap.org/data/2.5/weather";
+
+const API_KEY="c17d7ed3662a700dc3e17d1ab6e6c868";
+
+
+let getWeatherInfo = async () =>{
+    try{
+    let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+
+    let jsonResponse = await response.json();
+    // console.log(jsonResponse);
+    let result = {
+        city:city,
+        temp:jsonResponse.main.temp,
+        tempMin:jsonResponse.main.temp_min,
+         tempMax:jsonResponse.main.temp_max,
+         humidity:jsonResponse.main.humidity,
+         feelsLike:jsonResponse.main.feels_like,
+         weather:jsonResponse.weather[0].description,
+    };
+    console.log(result);
+    return result;
+}
+catch(err){
+    setError(true);
+}
+};
+
 
 
     let handleChange =(event) =>{
         setCity(event.target.value);
     };
 
-    let handleSubmit = (event)=>{
+    let handleSubmit = async (event)=>{
+        try{
         event.preventDefault();
         console.log(city);
-        setCity("");
-    }
+      let newInfo = await getWeatherInfo();
+      updateInfo(newInfo);
+      setCity("");
+        }
+        catch(err){
+            setError(true);
+        }
+    };
 
     return (
         <div className="SearchBox">
-            <h3>Search for the weather</h3>
             <form onSubmit={handleSubmit}>
                 <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange}/>
 
@@ -27,6 +63,7 @@ export default function SearchBox(){
                 <Button variant="contained" type="submit">
                 Search
                 </Button>
+                {error && <p>No Such Place Exists</p>};
             </form>
         </div>
     );
