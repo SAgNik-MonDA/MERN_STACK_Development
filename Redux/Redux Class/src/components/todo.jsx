fffd1423c2ec190186da339daf45d6a24080d6e8
sagnik mondal <sagnikmondal011@gmail.com>
2025-10-23 12:08:40 +0530

    modify 64 & add file

--- Redux/Redux Class/src/components/todo.jsx
+++ Redux/Redux Class/src/components/todo.jsx
@@ -1,17 +1,18 @@
 import { useSelector } from "react-redux";
-
-
-
+import AddForm from "./AddForm";
 
 export default function Todo(){
     const todos = useSelector((state) =>state.todos);
     console.log(todos);
     return (
         <>
+            <AddForm/>
             <h2>Todo List</h2>
 
             <ul>
-                {todos.map((todo) => (<li key={todo.id}>{todo.task}</li>))}
+                {todos.map((todo) => (<li key={todo.id}>{todo.task}
+                    <button>Delete</button>
+                </li>))}
             </ul>
         
         </>
