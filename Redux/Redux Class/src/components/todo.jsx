fab7f5ea0ebf43cdfa651bc55eaf8a129ed59aa2
sagnik mondal <sagnikmondal011@gmail.com>
2025-10-23 12:19:49 +0530

    modify 65 & add file

--- Redux/Redux Class/src/components/todo.jsx
+++ Redux/Redux Class/src/components/todo.jsx
@@ -1,9 +1,20 @@
 import { useSelector } from "react-redux";
 import AddForm from "./AddForm";
+import { useDispatch } from "react-redux";
+import { deleteTodo } from "../features/todo/todoSlice";
 
 export default function Todo(){
     const todos = useSelector((state) =>state.todos);
     console.log(todos);
+    const dispatch = useDispatch();
+    const clickHandler = (id) =>{
+        console.log("delete",id);
+        dispatch(deleteTodo(id));
+    };
+
+
+
+
     return (
         <>
             <AddForm/>
@@ -11,7 +22,7 @@ export default function Todo(){
 
             <ul>
                 {todos.map((todo) => (<li key={todo.id}>{todo.task}
-                    <button>Delete</button>
+                    <button onClick={()=>clickHandler(todo.id)}>Delete</button>
                 </li>))}
             </ul>
         
